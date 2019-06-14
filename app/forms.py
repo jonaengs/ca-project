f49d54033938b3cd5435c184e65a1dac092b0f51
Iver Brønstad <iverbb@stud.ntnu.no>
2019-06-14 12:35:40 +0200

    Forced non-empty fields, added text wraparound, margins

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
+    openid = StringField('openid', validators=[DataRequired(message=None)])
     remember_me = BooleanField('remember_me', default=False)
 
 class PostForm(Form):
-    title = StringField('Title')
-    user_name = StringField('user_name')
-    body  = StringField('Body')
+    title = StringField('Title', validators=[DataRequired(message=None)])
+    user_name = StringField('user_name', validators=[DataRequired(message=None)])
+    body  = StringField('Body', validators=[DataRequired(message=None)])
 
